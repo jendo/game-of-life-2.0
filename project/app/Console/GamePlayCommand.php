77f202229b85c60321c8576001aea13c17f9fd99
Michal Jenis <michal.jenis@kosik.cz>
2022-10-13 14:51:22 +0200

    Load xml file via FileLoader

--- project/app/Console/GamePlayCommand.php
+++ project/app/Console/GamePlayCommand.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Console;
 
+use App\Loader\FileIsIsNotReadableException;
+use App\Loader\FileLoader;
+use App\Loader\FileNotExistException;
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,6 +17,14 @@ final class GamePlayCommand extends Command
 {
     private const INPUT_ARG = 'i';
     private const OUTPUT_ARG = 'o';
+
+    private FileLoader $fileLoader;
+
+    public function __construct(FileLoader $fileLoader)
+    {
+        parent::__construct();
+        $this->fileLoader = $fileLoader;
+    }
 
     protected function configure(): void
     {
@@ -25,6 +37,36 @@ final class GamePlayCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        try {
+            $inputXml = $this->getInputXml($input);
+        } catch (InvalidArgumentException $e) {
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            return -1;
+        }
+
+        try {
+            $content = $this->fileLoader->load($inputXml);
+        } catch (FileIsIsNotReadableException | FileNotExistException $e) {
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            return -1;
+        }
+
         return 0;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    private function getInputXml(InputInterface $input): string
+    {
+        $inputXml = $input->getArgument(self::INPUT_ARG);
+        if (is_string($inputXml) === false) {
+            throw new InvalidArgumentException(
+                sprintf('Argument for input xml file must be string. Type %s provided', gettype($inputXml))
+            );
+        }
+
+        return $inputXml;
     }
 }
---
+++ project/tests/Unit/Loader/FileLoaderTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Unit\Loader;
+
+use App\Loader\FileIsIsNotReadableException;
+use App\Loader\FileLoader;
+use App\Loader\FileNotExistException;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class FileLoaderTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private FileLoader $fileLoader;
+
+    private string $filename;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fileLoader = new FileLoader();
+        $this->filename = __DIR__ . DIRECTORY_SEPARATOR . 'file.txt';
+    }
+
+    /**
+     * @throws FileNotExistException
+     * @throws FileIsIsNotReadableException
+     */
+    public function testLoadWithExistingFile(): void
+    {
+        $expectedContent = 'file content';
+        file_put_contents($this->filename, $expectedContent);
+        $content = $this->fileLoader->load($this->filename);
+
+        self::assertSame($expectedContent, $content, 'Expected content from file is not same like actual.');
+    }
+
+    /**
+     * @throws FileIsIsNotReadableException
+     */
+    public function testLoadWithNotExistingFile(): void
+    {
+        $fileName = 'dummy.txt';
+        $this->expectException(FileNotExistException::class);
+        $this->fileLoader->load(__DIR__ . DIRECTORY_SEPARATOR . $fileName);
+    }
+
+    /**
+     * @throws FileNotExistException
+     */
+    public function testLoadWithNotReadableFile(): void
+    {
+        file_put_contents($this->filename, '');
+        chmod($this->filename, 000);
+        $this->expectException(FileIsIsNotReadableException::class);
+        $this->fileLoader->load($this->filename);
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->filename) === true) {
+            unlink($this->filename);
+        }
+    }
+
+}
